15dbc35ef025ba366945ec2e15077e92fe78f54a
Saurabh Chauhan <saurabh2chauhanp@gmail.com>
2025-09-29 11:29:19 +0530

    Initial Commit

---
+++ Dark Mode Toggle/script.js
@@ -0,0 +1,24 @@
+const inputEl = document.querySelector(".input");
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked = JSON.parse(localStorage.getItem("mode") === "true");
+
+updateBody();
+
+function updateBody() {
+  if (inputEl.checked) {
+    bodyEl.style.backgroundColor = "black";
+  } else {
+    bodyEl.style.backgroundColor = "white";
+  }
+}
+
+inputEl.addEventListener("input", function () {
+  updateBody();
+  updateLocalStorage();
+});
+
+function updateLocalStorage() {
+  localStorage.setItem("mode", JSON.stringify(inputEl.checked));
+}
